2ca7380efc2a6e6835fc4e804886ef894130b9c1
unknown <minh.huang96@gmail.com>
2019-08-14 15:52:57 -0700

    small fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NASA from "./components/NASA";
 
+
 function App() {
   return (
     <div className="App">
       <div className="head">
         <h1>NASA Daily Photo Apps</h1>
       </div>
+      
       <NASA />
     </div>
   );
