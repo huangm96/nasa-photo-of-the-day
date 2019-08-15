20415b951c4ff4d424a8f01337af1490ba97ba71
unknown <minh.huang96@gmail.com>
2019-08-15 13:09:17 -0700

    css to styled-components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./App.css";
 import NASA from "./components/NASA";
+import styled from 'styled-components';
+
+const AppStyle = styled.div`
+     text-align: center;
+     max-width:100vw;
+`;
+
 
 
 function App() {
   return (
-    <div className="App">
-      <div className="head">
+    <AppStyle>
+      <div >
         <h1>NASA Daily Photo Apps</h1>
       </div>
       
       <NASA />
-    </div>
+    </AppStyle>
   );
 }
 
--- src/components/NASADailyPhotoCard.js
+++ src/components/NASADailyPhotoCard.js
@@ -1,18 +1,29 @@
 import React from "react";
+import styled from 'styled-components';
 
+const DailyPhoto = styled.img`
+     width:70%;
+`;
+const Date = styled.p`
+    color:red;
+`;
 
+const Explanation = styled.p`
+    margin: 30px auto;
+    width:80%;
+  `;
 
 const NASADailyPhotoCard = (props) => {
-    if (!props.image) 
-    return <h3>Loading...</h3>;
-    
+    if (!props.image)
+        return <h3>Loading...</h3>;
+
     return (
         <div>
 
-<p className ="date">{props.date}</p>
-<img className="dailyPhoto"src={props.image} alt="nasa"/>
-<h3>{props.title}</h3>
-<p className ="explanation">{props.explanation}</p>
+            <Date>{props.date}</Date>
+            <DailyPhoto src={props.image} alt="nasa" />
+            <h3>{props.title}</h3>
+            <Explanation>{props.explanation}</Explanation>
 
         </div>
 
